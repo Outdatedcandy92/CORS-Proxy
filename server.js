b67d0b141aabc5fbb68c5cac591e62745303cead
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-07-28 16:01:13 -0400

    please please work

--- server.js
+++ server.js
@@ -25,23 +25,9 @@ cors_proxy.createServer({
   originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
-  ],
+  removeHeaders: ['cookie', 'cookie2'],
   redirectSameOrigin: false,
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
 }).listen(port, host, function() {
