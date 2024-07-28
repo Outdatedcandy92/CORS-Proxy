f9b9cfcbf8fb1462fe39a4f3e3f20b9ac12b5fb5
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-07-28 13:57:42 -0400

    cors new deployment

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## Cors Proxy made for ![ArcadeTicket](https://github.com/Outdatedcandy92/ArcadeTicket)
+## cors proxy for arcade ticket counter
--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://cors-proxy-gray.vercel.app/jsonplaceholder.typicode.com/todos/1"
-headers = {
-    "Origin": "your-origin.com",  # Replace with your actual origin
-    "X-Requested-With": "XMLHttpRequest"
-}
-response = requests.get(url, headers=headers)
-print(response.text)
