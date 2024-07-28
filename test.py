f4c6f50093d4cbb08c9133b712aab1009af62d07
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-07-28 01:13:59 -0400

    something

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import requests
-url = "https://cors-proxy-inky.vercel.app/hackhour.hackclub.com/api/history/U079HV9PTC7"
+
+url = "https://cors-proxy-gray.vercel.app/jsonplaceholder.typicode.com/todos/1"
 headers = {
     "Origin": "your-origin.com",  # Replace with your actual origin
-    "X-Requested-With": "XMLHttpRequest",
-    "Authorization": "Bearer 5ee2ee3e-b72f-41e2-a038-0c06ffec0cf2"  # Replace with your actual authorization token
+    "X-Requested-With": "XMLHttpRequest"
 }
 response = requests.get(url, headers=headers)
 print(response.text)
